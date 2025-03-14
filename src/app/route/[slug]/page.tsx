c6aea3eaeae39d2855ad4c5345fd72a53c9210ca
Luka Smiljić <71632978+lukasmiljic@users.noreply.github.com>
2025-03-14 15:40:01 +0100

    adjust route page to pass deployment

--- src/app/route/[slug]/page.tsx
+++ src/app/route/[slug]/page.tsx
@@ -1,13 +1,19 @@
 import Layout from '@/components/Layout';
 
+type Params = Promise<{ slug: string }>;
+
 interface RoutePageProps {
-  params: { slug: string };
+  params: Params;
 }
 
-const RoutePage = ({ params }: RoutePageProps) => (
-  <Layout>
-    <p>Route page: {params.slug}</p>
-  </Layout>
-);
+const RoutePage = async ({ params }: RoutePageProps) => {
+  const { slug } = await params; // Resolve the promise
+
+  return (
+    <Layout>
+      <p>Route page: {slug}</p>
+    </Layout>
+  );
+};
 
 export default RoutePage;
