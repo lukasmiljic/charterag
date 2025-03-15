f7ceceb599d397c5dbacd5f7a2bc3f05ba710d99
Luka Smiljić <71632978+lukasmiljic@users.noreply.github.com>
2025-03-15 21:52:09 +0100

    add components to routes section

--- src/views/home/RoutesSection/RoutesSection.tsx
+++ src/views/home/RoutesSection/RoutesSection.tsx
@@ -1,8 +1,18 @@
+import RoutesCards from '@/components/RoutesCards';
+import SectionHeader from '@/components/SectionHeader';
+
 import styles from './RoutesSection.module.scss';
 
 const RoutesSection = () => (
   <section className={styles.container}>
-    <h2>Routes section</h2>
+    <div className={styles.sectionheadercontainer}>
+      <SectionHeader
+        caption="destinations"
+        header="Whether it's a bash or bliss — the choice is yours"
+        paragraph="On our customized sailing adventures, embracing the thrill of our party routes or unwinding on our relaxing voyages — crafting your perfect journey at sea has never been easier"
+      />
+    </div>
+    <RoutesCards />
   </section>
 );
 
