f6b79e899ae1473d46f2ef9f6866e85732077e1d
Luka Smiljić <71632978+lukasmiljic@users.noreply.github.com>
2025-03-16 21:32:14 +0100

    add payment section

--- src/views/home/PaymentSection/PaymentSection.tsx
+++ src/views/home/PaymentSection/PaymentSection.tsx
@@ -1,8 +1,45 @@
+import Caption from '@/components/Caption';
+import SectionHeader from '@/components/SectionHeader';
+
 import styles from './PaymentSection.module.scss';
 
 const PaymentSection = () => (
   <section className={styles.container}>
-    <h2>Payment section</h2>
+    <SectionHeader
+      caption="payment"
+      header="You don't have to break the bank"
+      paragraph="See how lorem ipsum dolor sit"
+    />
+    <div className={styles.cards}>
+      <div className={styles.card}>
+        <div className={styles.text}>
+          <h3>Sailweek experience</h3>
+          <Caption text="from 787€" regularCase backgroundColor="#062949" />
+        </div>
+        <ul>
+          <li>experience</li>
+          <li>lorem ipsum dolor</li>
+          <li>dolor sit et</li>
+          <li>lorem ipsum dolor</li>
+          <li>et santi dolor sum</li>
+          <li>ipsum lorem dolor sit et</li>
+        </ul>
+      </div>
+
+      <div className={styles.card} style={{ backgroundColor: 'white' }}>
+        <div className={styles.text}>
+          <h3>Individual experience</h3>
+        </div>
+        <ul>
+          <li>experience</li>
+          <li>lorem ipsum dolor</li>
+          <li className={styles.inactive}>dolor sit et</li>
+          <li className={styles.inactive}>lorem ipsum dolor</li>
+          <li className={styles.inactive}>et santi dolor sum</li>
+          <li className={styles.inactive}>ipsum lorem dolor sit et</li>
+        </ul>
+      </div>
+    </div>
   </section>
 );
 
