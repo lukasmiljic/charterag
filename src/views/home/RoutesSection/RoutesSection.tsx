403bddf22172f8e92f4c8a44b625b292183b0966
Luka Smiljić <71632978+lukasmiljic@users.noreply.github.com>
2025-03-17 23:19:12 +0100

    refactor RoutesCards and RoutesSection

--- src/views/home/RoutesSection/RoutesSection.tsx
+++ src/views/home/RoutesSection/RoutesSection.tsx
@@ -4,14 +4,13 @@ import SectionHeader from '@/components/SectionHeader';
 import styles from './RoutesSection.module.scss';
 
 const RoutesSection = () => (
-  <section className={styles.container}>
-    <div className={styles.sectionheadercontainer}>
-      <SectionHeader
-        caption="destinations"
-        header="Whether it's a bash or bliss — the choice is yours"
-        paragraph="On our customized sailing adventures, embracing the thrill of our party routes or unwinding on our relaxing voyages — crafting your perfect journey at sea has never been easier"
-      />
-    </div>
+  <section className={`${styles.routesSection} centercontainer`}>
+    <SectionHeader
+      className={styles.routesSectionHeader}
+      caption="destinations"
+      header="Whether it's a bash or bliss — the choice is yours"
+      paragraph="On our customized sailing adventures, embracing the thrill of our party routes or unwinding on our relaxing voyages — crafting your perfect journey at sea has never been easier"
+    />
     <RoutesCards />
   </section>
 );
