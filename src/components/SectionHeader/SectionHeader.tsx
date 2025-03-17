6da33d4b2e322226ef4a8b35bfb0e8593833affc
Luka Smiljić <71632978+lukasmiljic@users.noreply.github.com>
2025-03-17 22:57:13 +0100

    simplify className handling

--- src/components/SectionHeader/SectionHeader.tsx
+++ src/components/SectionHeader/SectionHeader.tsx
@@ -9,7 +9,7 @@ interface SectionHeaderProps {
 }
 
 const SectionHeader = ({ caption, header, paragraph, className }: SectionHeaderProps) => (
-  <div className={`${styles.text} ${className ? className : ''}`}>
+  <div className={`${styles.text} ${className || ''}`}>
     {caption && <Caption text={caption} />}
     <h2>{header}</h2>
     {paragraph && <p className={`${styles.paragraph} body2`}>{paragraph} </p>}
