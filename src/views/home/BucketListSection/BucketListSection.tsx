9e72a7e75c032ec60a36c2d467d126832f9a928c
Luka Smiljić <71632978+lukasmiljic@users.noreply.github.com>
2025-03-17 12:23:08 +0100

    add bucketlist section

--- src/views/home/BucketListSection/BucketListSection.tsx
+++ src/views/home/BucketListSection/BucketListSection.tsx
@@ -1,8 +1,45 @@
+import Image from 'next/image';
+
+import Button from '@/components/Button';
+import SectionHeader from '@/components/SectionHeader';
+
 import styles from './BucketListSection.module.scss';
 
 const BucketListSection = () => (
   <section className={styles.container}>
-    <h2>Bucketlist section</h2>
+    <div className={styles.images}>
+      <Image
+        className={styles.image1}
+        src="/sections/experience/explore.png"
+        alt="card image"
+        width={233}
+        height={349}
+      />
+      <Image
+        className={styles.image2}
+        src="/sections/bucketlist/image2.jpg"
+        alt="card image"
+        width={412}
+        height={274}
+      />
+      <Image
+        className={styles.image3}
+        src="/sections/bucketlist/image3.jpg"
+        alt="card image"
+        width={262}
+        height={273}
+      />
+    </div>
+    <div className={styles.sectionheadercontainer}>
+      <SectionHeader
+        header="Check Charterag off your bucket list"
+        paragraph="Lorem ipsum dolor sit amet consectetur. Tellus lectus non purus odio amet"
+      />
+    </div>
+    <div className={styles.buttons}>
+      <Button text="explore" />
+      <Button text="follow us" color="white" />
+    </div>
   </section>
 );
 
