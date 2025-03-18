c1d028ba9c14f491dfdced829c9232bfc44b846f
Luka Smiljić <71632978+lukasmiljic@users.noreply.github.com>
2025-03-18 10:50:52 +0100

    fix route hero section bg image

--- src/views/route/HeroSection/HeroSection.tsx
+++ src/views/route/HeroSection/HeroSection.tsx
@@ -2,9 +2,13 @@ import styles from './HeroSection.module.scss';
 
 const HeroSection = () => (
   <section className={styles.heroSection}>
-    <div className="centercontainer">
-      <p className="body1">Party route</p>
-      <h2>The best stories are yet to be told</h2>
+    <div className={styles.bgImage}>
+      <div className={`${styles.contentContainer} container`}>
+        <div className={`${styles.content}`}>
+          <p className="body1">Party route</p>
+          <h2>The best stories are yet to be told</h2>
+        </div>
+      </div>
     </div>
   </section>
 );
