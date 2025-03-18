2bd24a22ba815e571c10bad30d16479a84c61700
Luka Smiljić <71632978+lukasmiljic@users.noreply.github.com>
2025-03-18 15:20:54 +0100

    add basic carousel component

---
+++ src/components/Carousel/Carousel.tsx
@@ -0,0 +1,74 @@
+'use client';
+
+import { useState } from 'react';
+
+import Image from 'next/image';
+
+import styles from './Carousel.module.scss';
+
+interface CarouselProps {
+  images: string[];
+}
+
+const Carousel = ({ images }: CarouselProps) => {
+  const [currentImage, setCurrentImage] = useState(0);
+
+  const showNextImage = () => {
+    setCurrentImage(currentImage === images.length - 1 ? 0 : currentImage + 1);
+  };
+
+  const showPreviousImage = () => {
+    setCurrentImage(currentImage === 0 ? images.length - 1 : currentImage - 1);
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>, direction: 'next' | 'prev') => {
+    if (event.key === 'Enter' || event.key === ' ') {
+      event.preventDefault();
+      if (direction === 'next') {
+        showNextImage();
+      } else {
+        showPreviousImage();
+      }
+    }
+  };
+
+  return (
+    <div className={styles.carousel}>
+      <div className={styles.allImages}>
+        {images.map(url => (
+          <Image
+            className={styles.image}
+            key={url}
+            src={url}
+            alt="carousel image"
+            width={555}
+            height={500}
+            style={{ translate: `${-100 * currentImage}%` }}
+          />
+        ))}
+      </div>
+      <div className={styles.buttons}>
+        <div
+          className={`${styles.prev} ${styles.button}`}
+          role="button"
+          tabIndex={0}
+          onClick={showPreviousImage}
+          onKeyDown={event => handleKeyDown(event, 'prev')}
+        >
+          <Image className={styles.chevron} src="/icons/Chevron.svg" alt="" width={8} height={8} />
+        </div>
+        <div
+          className={`${styles.next} ${styles.button}`}
+          role="button"
+          tabIndex={0}
+          onClick={showNextImage}
+          onKeyDown={event => handleKeyDown(event, 'next')}
+        >
+          <Image className={styles.chevron} src="/icons/Chevron.svg" alt="" width={8} height={8} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
