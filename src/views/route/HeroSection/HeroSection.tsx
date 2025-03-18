3686ba4a72948191b9731e87fd149b3755a3c8b0
Luka Smiljić <71632978+lukasmiljic@users.noreply.github.com>
2025-03-18 11:01:19 +0100

    add text and padding to routes hero area

--- src/views/route/HeroSection/HeroSection.tsx
+++ src/views/route/HeroSection/HeroSection.tsx
@@ -10,6 +10,20 @@ const HeroSection = () => (
         </div>
       </div>
     </div>
+    <div className="container">
+      <div className={styles.text}>
+        <p className="subtitle">
+          From mountains and canyons to fjord-like bays, nostalgic Old towns to most stunning beaches. Get to now
+          Croatias twin sister — Montenegro
+        </p>
+        <p className="subtitle">
+          Walk the medieval streets of Kotor, dine the most amazing seafood in the Fishermans village. Feel like a
+          millionaire in Porto Montenegro, the most luxurious marina in this part of the World. Visit nostalgic Old
+          towns Perast, Risan and Herceg Novi. Hike mountains in Kotor. Get Wild in Budva and Tivat. Relax in freshly
+          opened, Chic Lazure marina.
+        </p>
+      </div>
+    </div>
   </section>
 );
 
