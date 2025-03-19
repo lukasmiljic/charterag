a8c68f4d44ce1c2f83fa23332045cac57633e28f
Luka Smiljić <71632978+lukasmiljic@users.noreply.github.com>
2025-03-19 17:28:39 +0100

    route cards are now dynamically loaded from file

--- src/components/RoutesCards/RoutesCards.tsx
+++ src/components/RoutesCards/RoutesCards.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
 import Bar from '@/components/Bar';
+import Routes, { Sections } from '@/data/routes';
 
 import styles from './RoutesCards.module.scss';
 
@@ -14,103 +15,40 @@ const RoutesCards = () => {
     router.push(route);
   };
 
+  const routeRoutes = Routes.filter(x => x.section.includes(Sections.Routes));
+
   return (
     <div className={styles.cards}>
-      <button type="button" className={styles.card} onClick={() => navigateTo('/route/party')}>
-        <Image className={styles.bgimage} src="/sections/routes/party.png" alt="card background image" fill />
-        <div className={styles.text}>
-          <div className={styles.nameandprice}>
-            <p className="subtitle">From €710/person</p>
-            <h4 className="semibold">Party route</h4>
-          </div>
-          <div className={styles.bars}>
-            <div className={styles.bararea}>
-              <p className="body1">Party</p>
-              <div className={styles.barcontainer}>
-                <Bar percentage={90} />
-              </div>
+      {routeRoutes.map(route => (
+        <button type="button" className={styles.card} onClick={() => navigateTo(`/route/${route.slug}`)}>
+          <Image
+            className={styles.bgimage}
+            src={route.primaryCoverImage || route.coverImage}
+            alt="card background image"
+            fill
+          />
+          <div className={styles.text}>
+            <div className={styles.nameandprice}>
+              <p className="subtitle">From €{route.price}/person</p>
+              <h4 className="semibold">{route.title} route</h4>
             </div>
-            <div className={styles.bararea}>
-              <p className="body1">Adventure</p>
-              <div className={styles.barcontainer}>
-                <Bar percentage={30} />
+            <div className={styles.bars}>
+              <div className={styles.bararea}>
+                <p className="body1">Party</p>
+                <div className={styles.barcontainer}>
+                  <Bar percentage={route.partyPercentage} />
+                </div>
               </div>
-            </div>
-          </div>
-        </div>
-      </button>
-
-      <button type="button" className={styles.card} onClick={() => navigateTo('/route/ultra')}>
-        <Image className={styles.bgimage} src="/sections/routes/ultra.png" alt="card background image" fill />
-        <div className={styles.text}>
-          <div className={styles.nameandprice}>
-            <p className="subtitle">From €792/person</p>
-            <h4 className="semibold">Ultra route</h4>
-          </div>
-          <div className={styles.bars}>
-            <div className={styles.bararea}>
-              <p className="body1">Party</p>
-              <div className={styles.barcontainer}>
-                <Bar percentage={95} />
-              </div>
-            </div>
-            <div className={styles.bararea}>
-              <p className="body1">Party</p>
-              <div className={styles.barcontainer}>
-                <Bar percentage={10} />
+              <div className={styles.bararea}>
+                <p className="body1">Adventure</p>
+                <div className={styles.barcontainer}>
+                  <Bar percentage={route.adventurePercentage} />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </button>
-
-      <button type="button" className={styles.card} onClick={() => navigateTo('/route/greece')}>
-        <Image className={styles.bgimage} src="/sections/routes/greece.png" alt="card background image" fill />
-        <div className={styles.text}>
-          <div className={styles.nameandprice}>
-            <p className="subtitle">From €792/person</p>
-            <h4 className="semibold">Greece route</h4>
-          </div>
-          <div className={styles.bars}>
-            <div className={styles.bararea}>
-              <p className="body1">Party</p>
-              <div className={styles.barcontainer}>
-                <Bar percentage={40} />
-              </div>
-            </div>
-            <div className={styles.bararea}>
-              <p className="body1">Adventure</p>
-              <div className={styles.barcontainer}>
-                <Bar percentage={80} />
-              </div>
-            </div>
-          </div>
-        </div>
-      </button>
-
-      <button type="button" className={styles.card} onClick={() => navigateTo('/route/adventure')}>
-        <Image className={styles.bgimage} src="/sections/routes/adventure.png" alt="card background image" fill />
-        <div className={styles.text}>
-          <div className={styles.nameandprice}>
-            <p className="subtitle">From €792/person</p>
-            <h4 className="semibold">Adventure route</h4>
-          </div>
-          <div className={styles.bars}>
-            <div className={styles.bararea}>
-              <p className="body1">Party</p>
-              <div className={styles.barcontainer}>
-                <Bar percentage={10} />
-              </div>
-            </div>
-            <div className={styles.bararea}>
-              <p className="body1">Adventure</p>
-              <div className={styles.barcontainer}>
-                <Bar percentage={90} />
-              </div>
-            </div>
-          </div>
-        </div>
-      </button>
+        </button>
+      ))}
     </div>
   );
 };
